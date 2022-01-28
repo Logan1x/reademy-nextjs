8e6a40b913fe9f8b5d1eafb312221c99d3a20cb8
logan1x <sharmakhushal78@gmail.com>
2022-01-28 13:54:22 +0530

    Inital Prototype(frontend only)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import InputForm from "./components/input";
+
 
 export default function Home() {
   return (
@@ -8,74 +10,44 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+      <div className=" md:w-2/5 mx-4 md:mx-auto ">
+      <h1 className="text-2xl font-semibold my-4">Monthly Reading Challenge</h1>
+      <p>
+        We have 4 different themes (will have different channels for each and
+        the idea is to read one book a month):
+      </p>
+      <ol className="my-2">
+        <li>
+          1. <span className="font-semibold">adulting</span> - anything that you
+          know will help you somehow, like atomic habits, the defining decade,
+          becoming, lean in, educated, etc.
+        </li>
+        <li>
+          2. <span className="font-semibold">escape</span> - classic
+          fiction/scifi go here
+        </li>
+        <li>
+          3. <span className="font-semibold">fun</span> - children's literature,
+          graphic novels, humor books{" "}
+        </li>
+        <li>
+          4. <span className="font-semibold">curiosity</span> - this is for avid
+          readers, picking up an out of comfort zone book, different opinion,
+          long/academic books like architecture books, game design, etc.
+        </li>
+      </ol>
+      <p>
+        I'd recommend to pick only 1 for now (thinking to put some monetary
+        friction to join multiple clubs at the same time).
+        {/* // TODO:  You'll get the link to join the community Discord server after you fill out this form. See you there! 👋 */}
+      </p>
+      <InputForm />
+    </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="ml-2 h-4" />
-        </a>
+        <p>Made By <span>Logan1x</span></p>
       </footer>
     </div>
   )
